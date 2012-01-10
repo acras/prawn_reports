b5753520d730d252a3eec0b3a3b7a033be713324
juliano_hs <juliano_hs@2579b3e0-257a-994a-8a5b-07e09b2024e3>
2012-01-10 19:32:13 +0000

    require que estava faltando.
    git-svn-id: http://svn.acras.com.br/ac_rubygems/prawn_reports/trunk@13954 2579b3e0-257a-994a-8a5b-07e09b2024e3

--- lib/prawn_report.rb
+++ lib/prawn_report.rb
@@ -3,6 +3,7 @@
 require "report"
 require "bands/band"
 require "bands/summary_band"
+require "active_record_helpers"
 require File.expand_path(File.dirname(__FILE__) + "/../repo/bands/headers/header_001.rb")
 require File.expand_path(File.dirname(__FILE__) + "/../repo/bands/footers/footer_001.rb")
 require File.expand_path(File.dirname(__FILE__) + "/../repo/reports/simple_listing.rb")
