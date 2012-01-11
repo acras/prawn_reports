6fbcea3e24bc41904e6942b4073abd6429ace059
juliano_hs <juliano_hs@2579b3e0-257a-994a-8a5b-07e09b2024e3>
2012-01-11 13:27:50 +0000

    filtros tratamento quando não há dados
    git-svn-id: http://svn.acras.com.br/ac_rubygems/prawn_reports/trunk@13958 2579b3e0-257a-994a-8a5b-07e09b2024e3

--- lib/custom_report_controller.rb
+++ lib/custom_report_controller.rb
@@ -22,14 +22,25 @@ module PrawnReportController
     @serialization_params || {}
   end
   
+  def get_pr_filters
+    @filters
+  end
+  
   def index
+    @filters = []
     rec = get_pr_report_data
-    report_content = rec.pr_serialize(get_pr_serialization_params)
-    report = get_pr_report_class.new(get_pr_report_params)
-    
-    report_content = report.draw(report_content.get_yaml)
+    puts rec.inspect
+    if rec.nil? || (rec.is_a?(Array) && rec.count == 0)
+      render :nothing => true, :status => :no_content 
+    else
+      report_content = rec.pr_serialize(get_pr_serialization_params)
+      puts report_content.get_yaml.inspect
+      report = get_pr_report_class.new(get_pr_report_params)
+      report.params[:filters] = get_pr_filters
+      report_content = report.draw(report_content.get_yaml)
 
-    send_data(report_content, :filename => get_pr_suggested_filename)
+      send_data(report_content, :filename => get_pr_suggested_filename)
+    end
   end
   
 end 
