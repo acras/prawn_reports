c7ff23a6a85b991aecdbb850b9c521f3a792ed4a
acras <acras@2579b3e0-257a-994a-8a5b-07e09b2024e3>
2012-03-18 22:07:46 +0000

    apagando duplicados
    git-svn-id: http://svn.acras.com.br/ac_rubygems/prawn_reports/trunk@14377 2579b3e0-257a-994a-8a5b-07e09b2024e3

--- generators/prawn_report_install/templates/add_system_criteria_to_ac_filter.rb
+++
@@ -1,9 +0,0 @@
-class AddSystemCriteriaToAcFilter < ActiveRecord::Migration
-  def self.up
-    add_column :ac_filters, :system_criteria, :string
-  end
-
-  def self.down
-    remove_column :ac_filters, :system_criteria
-  end
-end
