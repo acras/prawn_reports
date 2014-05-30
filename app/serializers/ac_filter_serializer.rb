d7da499cf08a6ba0dbbf87e179d5f789b14599a9
Vinicius Cubas Brand <viniciuscb@gmail.com>
2014-05-30 18:43:11 -0300

    added fields to filter serialization

--- app/serializers/ac_filter_serializer.rb
+++ app/serializers/ac_filter_serializer.rb
@@ -1,5 +1,5 @@
 class AcFilterSerializer < ActiveModel::Serializer
-  attributes :id, :data_type, :label, :target_model, :target_field, :required, :required_from, :required_to, :report_id
+  attributes :id, :data_type, :label, :target_model, :target_field, :required, :required_from, :required_to, :report_id, :query_user
 
   #Assumes that there is only one report to one ac_filter_def
   def report_id
