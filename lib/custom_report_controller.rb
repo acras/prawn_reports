26b43167ada165c7a75cd2f2c3f8c21610f3226e
Vinicius Cubas Brand <viniciuscb@gmail.com>
2014-08-15 12:20:05 -0300

    support for jQuery.fileDownload

--- lib/custom_report_controller.rb
+++ lib/custom_report_controller.rb
@@ -1,34 +1,34 @@
 #coding: utf-8
 
 module PrawnReportController
-  
+
   def get_pr_report_data
     []
   end
-    
+
   def get_pr_report_class
     @pr_report_class
   end
-    
+
   def get_pr_suggested_filename; end
-  
+
   def get_pr_report_params
     @pr_report_params || {}
   end
-  
+
   def get_pr_serialization_params
     @serialization_params || {}
   end
-  
+
   def get_pr_filters
     @filters
   end
-  
+
   def index
     @filters = []
     rec = get_pr_report_data
     if rec.nil? || (rec.is_a?(Array) && rec.count == 0)
-      render :nothing => true, :status => :no_content 
+      render :nothing => true, :status => :no_content
     else
       report_content = rec.pr_serialize(get_pr_serialization_params)
       report = get_pr_report_class.new(get_pr_report_params)
@@ -38,6 +38,7 @@ module PrawnReportController
 
       respond_to do |format|
         format.pdf do
+          cookies[:fileDownload] = true
           report_content = report.draw(report_content.get_yaml)
           if fn
             send_data(report_content, :filename => "#{fn}.pdf")
@@ -54,5 +55,5 @@ module PrawnReportController
 
     end
   end
-  
-end 
+
+end
